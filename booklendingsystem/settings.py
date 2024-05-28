9c9945f10c83c1ae713523ad83084ee7a8ee0449
White-Clouds <36442882+White-Clouds@users.noreply.github.com>
2024-05-28 21:55:17 +0800

    更改设置

--- booklendingsystem/settings.py
+++ booklendingsystem/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'library',
-        'USER': 'root',
+        'USER': 'library',
         'PASSWORD': '123456',
         'HOST': 'localhost',
         'PORT': '3307',
